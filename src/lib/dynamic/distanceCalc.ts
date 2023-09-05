60faf6def3a2048543a75a9ed57606239d490599
Shattik <1905008@ugrad.cse.buet.ac.bd>
2023-09-06 04:37:26 +0600

    routeApiAdded

---
+++ src/lib/dynamic/distanceCalc.ts
@@ -0,0 +1,47 @@
+let api_url: string =
+  "https://api.mapbox.com/directions-matrix/v1/mapbox/driving/";
+let api_key: string =
+  "access_token=pk.eyJ1Ijoic2hhdHRpayIsImEiOiJjbG02cW44bzkwbHVvM3FtbTVtM3F2anowIn0.HkzawX0-3a3geKX7LqooyA";
+let api_ano: string = "annotations=distance,duration";
+
+export async function distanceClac(
+  slat: number,
+  slng: number,
+  dlat: number,
+  dlng: number
+) {
+  try {
+    const response = await fetch(
+      api_url +
+        slng +
+        "," +
+        slat +
+        ";" +
+        dlng +
+        "," +
+        dlat +
+        "?" +
+        api_ano +
+        "&" +
+        api_key,
+      {
+        method: "GET",
+      }
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      return {
+        distance: data["distances"][0][1],
+        duration: data["durations"][0][1],
+      };
+    } else {
+      return {
+        status: response.status,
+        error: new Error("HTTP Error"),
+      };
+    }
+  } catch (err) {
+    throw err;
+  }
+}
