24d782316544a69964dc30cea2bbd4f4adf0d01b
BRAINIAC2677 <brainiac2677@gmail.com>
2023-09-07 02:02:33 +0600

    backend api integration

--- src/lib/stores/serverstore.ts
+++ src/lib/stores/serverstore.ts
@@ -1,6 +1,6 @@
 // to store the server url
 
 import { readable } from "svelte/store";
-const server_store = readable("http://localhost:3000");
+const server_store = readable("https://exploremate-express-api.vercel.app");
 
 export default server_store;
