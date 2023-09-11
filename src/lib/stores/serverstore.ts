e2338c14dc09dad256e409f06fbde09be70afb93
Sadat-Hossain-01 <sadatho2014@gmail.com>
2023-09-11 21:27:17 +0600

    restored the server url

--- src/lib/stores/serverstore.ts
+++ src/lib/stores/serverstore.ts
@@ -1,5 +1,5 @@
 import { readable, writable } from "svelte/store";
-const server_store = readable("http://localhost:3000"); // to store the server url
+const server_store = readable("https://exploremate-express-api.vercel.app");
 const loading_store = writable(false); // if any api fetch is in progress
 
 export default server_store;
